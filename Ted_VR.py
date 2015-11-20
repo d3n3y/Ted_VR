061b185b505fc4f45fb8a8af70389144642041af
d3n3y <deneyfletcher@gmail.com>
2015-11-20 11:39:28 +0300

    Breaks better. Updates to sensitivity

--- Ted_VR.py
+++ Ted_VR.py
@@ -1,18 +1,18 @@
+
 import speech_recognition as sr
 from speech_recognition import Microphone
 import pyaudio
 import time
 import pyttsx
 import random
-
-#Testing cloudmans edits
+import datetime
 
 def Playback(Audio):
     wf = Audio
     p = pyaudio.PyAudio()
     p.open
     stream = p.open(format=p.get_format_from_width(wf.sample_width),
-                    channels=wf.channels,
+                    channels=1,
                     rate=wf.sample_rate,
                     output=True)
 
@@ -38,110 +38,116 @@ def Google_VR(audio):
 
 class Results:
     def __init__(self):
-        self.greetings = ('hi','hello','Howdy')
+        self.greetings = ('hi','hello','howdy')
         self.whoami = ('your','name')
-        self.time =('time')
-        
-    def Response(self, command):
+        self.time = ('time')
+        self.timenow = datetime.datetime.now().strftime("%H:%M ")
+        self.weekday = datetime.datetime.now().strftime("%A")
+        self.date = datetime.datetime.now().strftime("%d %B %Y ")
 
+    def Response(self, command):
+        
         #Recieved a greeting
-        if command in self.greetings:
-            self.Hello(command)
+        for word in self.greetings:
+            if word in command:
+                self.Hello(command)
 
         #Probably asking for my name    
-        elif command in self.whoami:
+        if 'name' in command:
             self.MyName(command)
-
-        elif command in self.time:
-            #dotristansfunction
-
+        elif 'time' in  command:
+            self.say(self.timenow)
+        elif 'date' in command:
+            self.say(self.date)
+        elif 'day' in command:
+            self.say(self.weekday)
+        
             
         elif "ted" in command:
-            #if time in command:
-                #Time srft time module
-            #if date in command:
-                #
+            print 'in'
             if "lights" in command:
-                Lights(command)
+                self.Lights(command)
             if "thank you" in command:
                 Welcome(command)
         else:
             self.say('Unknown Command')
 
     def Hello(self, command):
+        print "in"
         #time of day greetings
         self.say(random.choice(self.greetings))
 
     def Welcome(self, command):    
-        print ("You are welcome!")
+        self.say("You are welcome!")
 
     def MyName(self, command):
         if "your" in command:
-            say('My name is ted')
-
-    ##def Name(command):
-    ##    if "my" and "is" in command:
-    ##        chop = command.index("is") + 3
-    ##        name = command[chop:]
-    ##        print "Hi", name, ", How are you?"
-    ##    if "what's my" in command:
-    ##        try:
-    ##            name
-    ##            print "You said your name is", name
-    ##        except:
-    ##            print "You didn't say your name :("
-
+            self.say('My name is ted')
+            
     def Lights(self, command):
         if 'down' in command:
-            say("Turning down the lights")
+            self.say("Turning down the lights")
         if 'off' in command:
-            say("Turning off the lights")
+            self.say("Turning off the lights")
             
         if 'up' in command:
-            say("Turning the lights up")
+            self.say("Turning the lights up")
         if 'on' in command:
-            say("Turning on the lights")
+            self.say("Turning on the lights")
 
 
-    def say(self, text):
-       print ('[!]', text)
-       #engine.say(text)
-       #engine.runAndWait()
+    def say(self, text,):
+        print '[!]', text
+        engine.say(text)
+        engine.runAndWait()
+
+    def gettime(self, command):
+        self.say(datetime.date.today().ctime())
 
 
 
 #INIT
 
 #Set up his voice
-#engine = pyttsx
-#engine.setProperty('rate', 120)
-#ted = engine.engine.getProperty('voices')[0]
-#engine.engine.setProperty('voice', ted.id)
+engine = pyttsx.init()
+engine.setProperty('rate', 120)
+ted = engine.getProperty('voices')[0]
+engine.setProperty('voice', ted.id)
 
 #Listen for us passively
 Recog = sr.Recognizer()
-Mic = sr.Microphone()
-Recog.energy_threshold = 5000
-command = None
+Mic = sr.Microphone(sample_rate = 10000)
+
+Recog.energy_threshold = 300
+#how long to wait after last word
+Recog.pause_threshold = 0.7
+#don't change the sensitivity over time
+Recog.dynamic_energy_threshold = False
+# how great the distance is between back. noise and voice. 1 is even 2 is double
+Recog.dynamic_energy_adjustment_ratio = 1.5
+
 
 #Setup our Results Class
 results = Results()
+x=0
 
 #Inteperet voice if we find it
 with Mic as source:
     while True: 
         try:
-            print ("[!] Listening")
-            audio = Recog.listen(source, 10)
-        except:
-            sr.WaitTimeoutError
-            audio = None
-        if audio != None:
-            print ("[!] Heard something. ")
-            command = Google_VR(audio)
-            if type(command) == unicode:
-                if command not in ('stop','goodnight'):
+            try:
+                x += 1
+                print ("[!] Listening " + str(x))
+                audio = Recog.listen(source,1)
+            except sr.WaitTimeoutError:
+                audio = None
+            if audio:
+                x=0
+                print ("[!] Heard something. ")
+                #Playback(audio)
+                command = Google_VR(audio)
+                if type(command) == unicode:   
                     results.Response(command)    
-                else:
-                    say('Goodbye')
-                    break
+        except KeyboardInterrupt:
+            print "[!] Exiting"
+            break
